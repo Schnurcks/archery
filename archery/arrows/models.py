813804d848cede799752617bf9a93bf19881f3c3
Webmaster Schnurcks <Webmaster@schnurcks.de>
2024-06-05 12:35:58 +0200

    last local commit

--- archery/arrows/models.py
+++ archery/arrows/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-
-
 class Arrow(models.Model):
     # Represents a complete arrow modell
-    title = models.CharField()
+    title = models.CharField(max_length=50)
     foc = models.FloatField()
     total_weight = models.FloatField()
 
@@ -15,6 +13,9 @@ class ArrowComponentType(models.Model):
     # Contains possible types of arrow components 
     #   e.g. shaft, feather, ...
     label = models.CharField(max_length=30) 
+
+    def __str__(self):
+        return self.label
 
 class ArrowComponentPropertyType(models.Model):
     # Contains possibke properties of a specific component type
@@ -27,17 +28,27 @@ class ArrowComponentPropertyType(models.Model):
         GRAIN_PER_INCH = "gn/in" 
     
     unit = models.CharField(max_length=5, choices=PropertyUnit, null=True)
+    
+    def __str__(self):
+        return f'{self.label} ({self.unit})'
 
 class ArrowComponent(models.Model):
-    # Possible arrow components 
-    type = models.ForeignKey(ArrowComponentType)
+    # Specific arrow components 
+    type = models.ForeignKey(ArrowComponentType, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return {self.type}
 
 class ArrowComponentProperty(models.Model):
     # Property of an arrow component
     arrow_component = models.ForeignKey(ArrowComponent, on_delete=models.CASCADE)
     property_type = models.ForeignKey(ArrowComponentPropertyType, on_delete=models.PROTECT)
     unit_value = models.FloatField(null=True)
-    non_unit_value = models.CharField(null=True)
+    non_unit_value = models.CharField(max_length=20, null=True)
+
+    
+    def __str__(self):
+        return {self.arrow_component}
 
 class ArrowComponentRelation(models.Model):
     # Lists Describes the properties of an arrow
@@ -49,8 +60,12 @@ class ArrowComponentRelation(models.Model):
         SHAFT_BACK = "BACK"   
 
     position_start = models.CharField(max_length=5, choices=PositionStart, default=PositionStart.SHAFT_FRONT) # either the component label or an own label 
-    position_distance_mm = models.DecimalField() # position measured from the end of the arrow shaft 
+    position_distance_mm = models.FloatField() # position measured from the end of the arrow shaft 
 
-class KnowHow_Arcticle(models.Model):
-    title = models.CharField()
-    content = models.TextField()
+class KnowHowArticle(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()#
+
+    
+    def __str__(self):
+        return {self.title}
